26e5889b9f215923d15c2619c548b377b7cfa778
BoBynou <90771596+BoBynou@users.noreply.github.com>
2023-07-07 09:23:17 +0200

    Pour lélé

--- CrAimeWeb/wwwroot/js/Modal.js
+++ CrAimeWeb/wwwroot/js/Modal.js
@@ -1,13 +1,13 @@
 ﻿// Get the modal
-var createModal = document.getElementById("createModal");
-var editModal = document.getElementById("editModal");
+var createModal = document.getElementById("eventCreateModal");
+var editModal = document.getElementById("eventEditModal");
 
 // Get the button that opens the modal
 var addButton = document.querySelector(".add-button");
-var editButton = document.querySelector(".edit-button");
+var editButtons = document.querySelectorAll(".edit-button");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spans = document.querySelectorAll(".close");
 
 // When the user clicks the button, open the modal
 addButton.onclick = function () {
@@ -15,18 +15,26 @@ addButton.onclick = function () {
 }
 
 // When the user clicks the button, open the modal
-editButton.onclick = function () {
-    editModal.style.display = "flex";
-}
+editButtons.forEach(function (editButton) {
+    editButton.onclick = function () {
+        editModal.style.display = "flex";
+    }
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    createModal.style.display = "none";
-}
+// when the user clicks on <span> (x), close the modal
+spans.forEach(function (span) {
+    span.onclick = function () {
+        createModal.style.display = "none";
+        editModal.style.display = "none";
+    }
+});
 
 // When the user clicks anywhere outside the modal, close it
 window.onclick = function (event) {
     if (event.target == createModal) {
         createModal.style.display = "none";
+    }
+    if (event.target == editModal) {
+        editModal.style.display = "none";
     }
 }
